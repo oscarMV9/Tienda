3e8eb8d9ab286ae915f043c97feb12c3b4bb8303
Oscar <oscarmontoya119@gmail.com>
2024-11-14 22:20:14 -0500

    terminado Dto

--- src/main/java/com/nogrup/celulares/Controller/OrdenController.java
+++ src/main/java/com/nogrup/celulares/Controller/OrdenController.java
@@ -5,9 +5,7 @@ import com.nogrup.celulares.Service.OrdenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,29 @@ public class OrdenController {
     public ResponseEntity<List<OrdenDto>> ordenGet() {
         List<OrdenDto> orden = serviciOrden.findAll();
         return new ResponseEntity<>(orden, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrdenDto> buscarPorId(@PathVariable Long id) {
+        OrdenDto ordenDto = serviciOrden.findById(id);
+        return ordenDto != null ? new ResponseEntity<>(ordenDto, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrdenDto> createOrdenes(@RequestBody OrdenDto ordenDto) {
+        OrdenDto newOrder = serviciOrden.save(ordenDto);
+        return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminarOrden(@PathVariable Long id) {
+        OrdenDto ordenDto = serviciOrden.findById(id);
+        if (ordenDto == null) {
+            return new ResponseEntity<>("la orden no se encuentra", HttpStatus.NOT_FOUND);
+        }
+        serviciOrden.delete(id);
+        return new ResponseEntity<>("la orden se elimino exitosamente", HttpStatus.OK);
     }
 
 }
